b708213f2e202bc859183f3c61679d119605e287
TheSidSpears <zeroly@ya.ru>
2016-05-18 19:39:30 +0300

    Убран запуск конфигов (теперь он в Route.php). Добавлен запуск Route

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -2,34 +2,15 @@
 //Действия, запускаемые всегда
 error_reporting(-1);
 mb_internal_encoding('utf-8');
-//header('Content-Type: text/html; charset=utf-8');
 
-try{
-	//Считываем конфиг
-	if(!file_exists('config.json'))
-		throw new Exception('Файла config.json не существует');
 
-	$config=file('config.json',FILE_IGNORE_NEW_LINES);
- 	$config=implode('',$config);
-	$config=json_decode($config);
-	
-	//Автозагрузка всех классов
-	function __autoload($class_name){
-		require_once("app/class/".$class_name.".php");
-	}
-	
-	/*
-		В будущем вместо строк ниже тут должна распологаться решение,
-		какую модель подключить и какой вид показать.
-		А еще лучше вывести это в отдельный файл, типа route.php
-	*/
-	//Подключаем модель
-	include('public/list.php');	
-	//Подключаем вид
-	include("views/list.php");
-}
-catch(Exception $e){
-	echo "Исключение: ", $e->getMessage(),"\n";
+function url($url){
+	return 'index.php?'.$url;
 }
 
-
+//Автозагрузка всех классов/моделей
+//Добавить try/catch на file_exists
+function __autoload($className){
+	require_once("app/class/".$className.".php");
+}
+Route::start();
