707fd22c725b50f70834b572717dfc899e06e045
TheSidSpears <zeroly@ya.ru>
2016-05-18 19:41:46 +0300

    Добавлено несколько видов

--- public/list.php
+++
@@ -1,6 +0,0 @@
-<?php
-//Соединяемся с базой
-$table=new StudentDataGateway($config->db); //может быть это нужно в bootstrap?
-
-//возвращает массив, где каждый students[] - объект Student
-$students=$table->getStudents();
